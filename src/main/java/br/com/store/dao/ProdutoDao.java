57c26e5250413853e32d7200f26732560e060433
Amanda Marques <amandamarqueslinger@gmail.com>
2026-06-18 18:56:59 -0300

    adicionando métodos

--- src/main/java/br/com/store/dao/ProdutoDao.java
+++ src/main/java/br/com/store/dao/ProdutoDao.java
@@ -3,6 +3,7 @@ package br.com.store.dao;
 import br.com.store.modelo.Produto;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class ProdutoDao {
@@ -24,4 +25,27 @@ public class ProdutoDao {
         String jpql = "SELECT p FROM Produto p"; //salvando o comando em uma variavel
         return em.createQuery(jpql , Produto.class).getResultList(); //aplicando a query e mostrando o resultado
     }
+
+    public List<Produto> buscarPorNome(String nome){
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome"; //passando o paramentro :nome para a busca
+        return em.createQuery(jpql , Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public List<Produto> buscarPorNomeDaCategoria(String nome){
+        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome"; //realizando o join automatico
+        return em.createQuery(jpql , Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    public BigDecimal buscarPrecoDoProdutoPorNome(String nome){
+        String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome"; //retornando apenas o preço
+        return em.createQuery(jpql , BigDecimal.class)
+                .setParameter("nome", nome)
+                .getSingleResult();// metodo para carregar apenas 1 unico registro
+    }
+
+
 }
--- src/main/java/br/com/store/testes/CadastroDeProduto.java
+++ src/main/java/br/com/store/testes/CadastroDeProduto.java
@@ -20,8 +20,12 @@ public class CadastroDeProduto {
         Produto p = produtoDao.buscarPorId(1L);
         System.out.println(p.getPreco());
 
-        List<Produto> todos = produtoDao.buscarTodos();
+        List<Produto> todos = produtoDao.buscarPorNome("Xiaomi Redmi");
+        List<Produto> todos2 = produtoDao.buscarPorNomeDaCategoria("CELULARES");
         todos.forEach( p2 -> System.out.println(p.getNome()));
+
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoPorNome("Xiaomi Redmi");
+        System.out.println("Preco do produto: " + precoDoProduto);
     }
 
     private static void cadastrarProduto() {
